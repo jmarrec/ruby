d999b639f1fa5f63d28fea9d88a644dd243b9040
Lars Kanis <lars@greiz-reinsdorf.de>
2021-06-10 21:10:33 +0200

    Don't add an exe file manifest
    It is later added as part of the rubyinstaller2 build process.
    The manifest was introduced in: https://github.com/ruby/ruby/pull/4505

--- win32/resource.rb
+++ win32/resource.rb
@@ -61,7 +61,6 @@ end
 #include <winver.h>
 
 #{icon || ''}
-#{type == 'VFT_APP' ? "1 RT_MANIFEST ruby.manifest" : ""}
 VS_VERSION_INFO VERSIONINFO
  FILEVERSION    #{nversion}
  PRODUCTVERSION #{nversion}
